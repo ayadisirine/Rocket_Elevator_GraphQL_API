fcbe5f3e0c158f3b5c2e7d8c36f6e36d3b48a394
YoannTessuer <yoann.tessier@hotmail.com>
2021-03-18 19:58:10 -0400

    Unmounted GraphiQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # if Rails.env.development?
   # mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "graphql#execute"
   post "/graphql", to: "graphql#execute"
 end
