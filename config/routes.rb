733efae408e13b304c6eab12b7f1311d9572f219
YoannTessuer <yoann.tessier@hotmail.com>
2021-03-18 16:38:25 -0400

    Mounted GraphQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "graphql#execute"
   post "/graphql", to: "graphql#execute"
 end
 
 # if Rails.env.development?
-#   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "graphql#execute"
 # end
