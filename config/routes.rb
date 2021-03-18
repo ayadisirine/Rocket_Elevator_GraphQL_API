695db93ecb1ed4725cdbea8509e344f2d5e75e85
YoannTessuer <yoann.tessier@hotmail.com>
2021-03-18 16:17:30 -0400

    Added GraphQL Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  post "/graphql", to: "graphql#execute"
 end
 
 # if Rails.env.development?
 #   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: "graphql#execute"
 # end
-# post "/graphql", to: "graphql#execute"
